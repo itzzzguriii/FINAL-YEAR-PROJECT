ac1eeba8c8dcc6451bbb7e3fb02110fb7ee17cf4
itzzzguriii <124148865+itzzzguriii@users.noreply.github.com>
2024-01-10 16:29:28 +0000

    head nav bar

---
+++ include/navbar.php
@@ -0,0 +1,8 @@
+<ul>
+    <li><a id="nav_home" href="home.php">Home</a></li>
+    <li><a id="nav_evSearch" href="evSearch.php">EV Search</a></li>
+    <li><a id="nav_security" href="security.php">Security</a></li>
+    <li><a id="nav_contact" href="contact.php">Contact</a></li>
+    <li><a id="nav_about" href="about.php">About</a></li>
+    <li style="float:right"><a href="api/logout.php">Logout</a></li>
+</ul>
