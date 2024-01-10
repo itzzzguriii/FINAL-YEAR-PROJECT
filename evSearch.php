1f7f546d55a2b5280ff76c8204311899e830749f
itzzzguriii <124148865+itzzzguriii@users.noreply.github.com>
2024-01-10 16:31:46 +0000

    search page

---
+++ evSearch.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+if(!isset($_SESSION['UserId']) || empty($_SESSION['UserId'])){
+    echo '<script>location.href = \'login.php\';</script>';
+}
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<?php include('include/head.php'); ?>
+<body class="normalBody" onload="setActive('nav_evSearch')">
+    <?php include('include/navbar.php'); ?>
+    <script src="js/script.js"></script>
+</body>
+</html>
